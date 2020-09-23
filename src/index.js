bec90b6c643037ed50dcd73815508b8359f97c03
Steve Anderson <prototype909@msn.com>
2020-09-23 12:28:17 -0600

    installed front end dependencies

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import usersReducer from './reducers/users'
+import store from './store'
 
-const reducer = combineReducers({
-  users: usersReducer
-})
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
-
-ReactDOM.render(
+ReactDOM.render(	ReactDOM.render(
   <Provider store={store}>
-      <App />
-  </Provider>, 
-document.getElementById('root'));
-
-
