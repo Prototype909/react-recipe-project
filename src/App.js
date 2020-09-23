87636abd2aa554310243de8e86544d35ed6ceb42
Steve Anderson <prototype909@msn.com>
2020-09-23 17:51:28 -0600

    navbar, layout, home changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Home } from './components/Home'
+import { NoMatch } from './components/NoMatch'
+import Login from './components/Login'
+import { Layout } from './components/Layout'
+import { NavBar } from './components/NavBar'
+import { Jumbotron} from './components/Jumbotron'
+import { connect } from 'react-redux'
+
 
 class App extends React.Component {
   render () {
     return (
-      <div className="App">
-        App
-        </div>
+      <React.Fragment>
+        <NavBar />
+        <Jumbotron />
+        <Layout>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+              <Route component={NoMatch} />
+
+              {/* <Route path="/signup" component={Signup} /> */}
+            </Switch>
+          </Router>
+        </Layout>
+      </React.Fragment>
     );
   }
+
 }
 
 
--- src/store.jsx
+++ src/store.jsx
@@ -4,10 +4,10 @@ import thunk from 'redux-thunk'
 
 const reducer = combineReducers({
     users: usersReducer
-  })
-  
-  const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  
-  const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+})
 
-  export default store
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+export default store
