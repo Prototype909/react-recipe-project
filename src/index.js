04214175f80633e3a8a2c107a25cfea814dc72a2
Steve Anderson <prototype909@msn.com>
2020-09-23 12:29:23 -0600

    installed frontend dependencies

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import App from './App';
 import { Provider } from 'react-redux'
 import store from './store'
 
-ReactDOM.render(	ReactDOM.render(
+ReactDOM.render(
   <Provider store={store}>
+    <App />
+    </Provider>,
+    document.getElementById('root')
+);
