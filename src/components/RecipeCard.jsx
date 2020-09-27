33cfe37c694af0aa847787cbd8d0ddb2bf9daad0
Steve Anderson <prototype909@msn.com>
2020-09-27 16:20:56 -0600

    added link, recipe and show

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 
 const RecipeCard = ({ recipe }) => {
     return (
-        <p>{recipe.attributes.name}</p>
+        recipe ?
+        <div>
+            <h4>{recipe.attributes.name}</h4>
+            <p>{recipe.attributes.imageUrl}</p>
+            <p>{recipe.attributes.description}</p>
+            <br />
+            <h5>Ingredients:</h5>
+            {recipe.attributes.ingredients.map((ing, idx) => {
+                return (
+                    <p key={idx}>
+                        {ing.quantity} {ing.unit} {ing.name}
+                    </p>
+                )
+            })}
+            <h5>Instructions:</h5>
+            <p>{recipe.attributes.instructions}</p>
+            <Link to={`/recipes/${recipe.id}/edit`}>Edit This Recipe</Link>
+
+        </div> :
+        <p>This is a recipe card with no recipe</p>
+        
     )
 }
 
-export default RecipeCard 
+export default RecipeCard
